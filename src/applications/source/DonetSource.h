e930d54a72035937ab9dcd22201521698c274290
giang <nguyen@cs.tu-darmstadt.de>
2012-09-04 11:00:43 +0200

    remove unused variables

--- src/applications/source/DonetSource.h
+++ src/applications/source/DonetSource.h
@@ -3,10 +3,8 @@
 
 #include "DonetBase.h"
 #include "AppCommon.h"
-//#include "ActivePeerTable.h"
-//#include "BufferMap.h"
 
-// TODO if the peer doesn't respond during an interval, remove it from list
+// TODO-Giang if the peer doesn't respond during an interval, remove it from list
 class DonetSource : public DonetBase
 {
 public:
@@ -18,37 +16,22 @@ protected:
     virtual void initialize(int stage);
     virtual void finish();
 
-// Helper functions
-private:
-	virtual void processPacket(cPacket *pkt);
+    virtual void processPacket(cPacket *pkt);
 	void handleTimerMessage(cMessage *msg);
 
+private:
 	// -- Partnership Management
 	void processPartnershipRequest(cPacket *pkt);
-//	void acceptJoinRequestFromPeer(IPvXAddress &reqPeerAddress);
 	void acceptJoinRequestFromPeer(IPvXAddress &reqPeerAddress, double bw);
-//	void sendBufferMap();
-	bool canHaveMorePartner(void);
+	//bool canHaveMorePartner(void);
 
-// Member data
 private:
     // -- Timer
     cMessage *timer_startVideo;
     cMessage *timer_newChunk;
-//    MeshBufferMapPacket *m_bmPacket;
 
     int totalBytesUploaded;
     int totalBytesDownloaded;
-
-    // new ones
-//    SEQUENCE_NUMBER_T m_id_bmStart; // smallest id in the BM
-//    SEQUENCE_NUMBER_T m_id_bmEnd; // biggest id in the BM
-    SEQUENCE_NUMBER_T m_id_newChunk;
-
-//   BufferMap *m_bufferMap;
-
-   // Timer, getting value from parameters or calculated from parameters
-   double m_newChunkTimerInterval;
 
 };
 
