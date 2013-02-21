7cb93e4aab0462e64ba38c82e10ba8c34971c10d
Giang Nguyen <nguyen@cs.tu-darmstadt.de>
2013-02-21 05:07:22 +0100

    added PlayerBase.cc and move getter for localCI up to the base class

--- src/payload/PlayerBase.h
+++ src/payload/PlayerBase.h
@@ -15,7 +15,6 @@
 //#define EVENT_CHUNK_IN_BUFFER       1
 
 class PlayerBase : public cSimpleModule {
-//class PlayerBase {
 public:
     PlayerBase() {};
     virtual ~PlayerBase() {};
@@ -32,71 +31,13 @@ public:
     virtual SEQUENCE_NUMBER_T getCurrentPlaybackPoint(void) = 0;
     virtual bool playerStarted(void) = 0;
 
-//    virtual long int getCountChunkHit(void) = 0;
-//    virtual long int getCountChunkMiss(void) = 0;
-
     virtual int getPlayerState(void) = 0;
 
     inline long int getCountChunkHit(void) { return m_count_chunkHit; }
     inline long int getCountChunkMiss(void) { return m_count_chunkMiss; }
 
-    //inline long int getCountChunkHit(void) { return m_countChunkHit; }
-    //inline long int getCountChunkMiss(void) { return m_countChunkMiss; }
-
-/*
-protected:
-    virtual int numInitStages() const { return 4; }
-    virtual void initialize(int stage);
-    virtual void finish();
-
-    virtual void handleMessage(cMessage *msg);
-    void handleTimerMessage(cMessage *msg);
-
-private:
-    bool shouldResumePlaying(SEQUENCE_NUMBER_T seq_num);
-
-protected:
-    // Pointer to other modules
-    //DonetPeer *m_
-
-    // -- State variable
-    bool m_playerStarted;
-    cMessage *timer_nextChunk;
-    cMessage *timer_playerStart;
-    SEQUENCE_NUMBER_T m_id_nextChunk;
-
-    // -- Module (secondary) parameters
-    double m_interval_newChunk;
-    double param_interval_recheckVideoBuffer;
-    int m_chunkSize; // Bytes
-
-   // -- Some new parameters for the Finite State Machine
-    int m_state;
-    double param_percent_buffer_low, param_percent_buffer_high;
-    int param_max_skipped_chunk;
-    double param_interval_probe_playerStart;
-
-    // -- Statistics locally
-    long int m_countChunkHit, m_countChunkMiss;
-
-    int m_skip;
-
-    // -- Pointers to external modules
-    VideoBuffer *m_videoBuffer;
-    AppSettingDonet *m_appSetting;
-    GlobalStatistic *m_stat;
-
-    // -- Signal for data collection
-    simsignal_t sig_chunkHit;
-    simsignal_t sig_chunkMiss;
-    simsignal_t sig_chunkSeek;
-    simsignal_t sig_rebuffering_local;
-
-    simsignal_t sig_rebuffering;
-    simsignal_t sig_stall;
-*/
 protected:
     long int m_count_chunkHit, m_count_chunkMiss;
 };
 
-#endif /* PLAYER_H_ */
+#endif /* PLAYERBASE_H_ */
