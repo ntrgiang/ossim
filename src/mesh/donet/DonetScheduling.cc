b0d90be6b847c1c6dbe302a67eb27d84b200a885
Giang Nguyen <nguyen@cs.tu-darmstadt.de>
2013-02-26 18:27:38 +0100

    cleaned up unnecessary codes from DonetScheduling.cc

--- src/mesh/donet/DonetScheduling.cc
+++ src/mesh/donet/DonetScheduling.cc
@@ -39,11 +39,9 @@ void DonetPeer::donetChunkScheduling(void)
     // double current_time = simTime().dbl();
 
     // -- Calculate the available for _all_ chunk (expect redundancy, but for simplicity of implementation), for _all_ partners
-    //m_partnerList->resetAllAvailableTime(m_videoBuffer->getBufferStartSeqNum(), lower_bound, m_videoBuffer->getChunkInterval());
     m_partnerList->resetAllAvailableTime(m_player->getCurrentPlaybackPoint(), lower_bound, m_videoBuffer->getChunkInterval());
 
     // -- Update bounds of all sendBM
-    // m_partnerList->updateBoundSendBm(lower_bound, lower_bound+m_bufferMapSize_chunk-1);
     m_partnerList->clearAllSendBm();
     m_partnerList->updateBoundSendBm(lower_bound, lower_bound+m_bufferMapSize_chunk-1);
     m_partnerList->resetNChunkScheduled();
@@ -54,6 +52,7 @@ void DonetPeer::donetChunkScheduling(void)
     {
        if (should_be_requested(seq_num) == false)
        {
+          //EV << "----> This chunk should be requested this time" << endl;
           continue;
        }
 
@@ -81,7 +80,6 @@ void DonetPeer::donetChunkScheduling(void)
     // -- Have a copy of the expected_set, so that chunks which has been found a supplier will be deleted from this copy
 
     // -- Browse through the expected_set
-    //for(std::vector<SEQUENCE_NUMBER_T>::iterator iter = expected_set.begin(); iter != expected_set.end(); ++iter)
     for (int i = 0; i < sizeExpectedSet; ++i)
     {
         SEQUENCE_NUMBER_T seq_num = expected_set[i];
@@ -103,64 +101,51 @@ void DonetPeer::donetChunkScheduling(void)
         {
            ++count_1;
 
-//            if (should_be_requested(seq_num) == true)
-//            {
-                //EV << "----> This chunk should be requested this time" << endl;
+           // -- Get pointer to the respective NeighborInfo
 
-                // -- Get pointer to the respective NeighborInfo
-                m_partnerList->setElementSendBm(holderList[0], seq_num, true);
-//                NeighborInfo *nbr_info = m_partnerList->getNeighborInfo(holderList[0]); // get obsolete?
+           // -- Set the respective element in the SendBm to say that this chunk should be requested
+           m_partnerList->setElementSendBm(holderList[0], seq_num, true);
 
-                // -- Set the respective element in the SendBm to say that this chunk should be requested
-                //nbr_info->setElementSendBm(expected_set[i], true);
-//                nbr_info->setElementSendBm(seq_num, true); // get obsolete?
+           m_list_requestedChunk.push_back(seq_num);
+           ++m_nChunkRequested_perSchedulingInterval;
 
-                m_list_requestedChunk.push_back(seq_num);
-                ++m_nChunkRequested_perSchedulingInterval;
+           // -- Get peer's upload bandwidth
 
-                // -- Get peer's upload bandwidth
+           //double peerUpBw = nbr_info->getUpBw(); // get obsolete?
+           double peerUpBw = m_partnerList->getUpBw(holderList[0]);
 
-                //double peerUpBw = nbr_info->getUpBw(); // get obsolete?
-                double peerUpBw = m_partnerList->getUpBw(holderList[0]);
+           bool ret = true;
+           // -- Browse through the expected_set
+           int currentSize = copied_expected_set.size();
+           for (int k = 0; k < currentSize; ++k)
+           {
+              // EV << "nHolder = 1; k = " << k << endl;
+              ret = m_partnerList->updateChunkAvailTime(holderList[0],
+                                                        copied_expected_set[k],
+                                                        (param_chunkSize*8)/peerUpBw);
+              if (ret == false)
+              {
+                 EV << "updateChunkAvailTime == false" << endl;
+                 break;
+              }
+           } // for
 
-                bool ret = true;
-                // -- Browse through the expected_set
-                int currentSize = copied_expected_set.size();
-                for (int k = 0; k < currentSize; ++k)
-                {
-                   // EV << "nHolder = 1; k = " << k << endl;
-                   // ret = nbr_info->updateChunkAvailTime(copied_expected_set[k], (param_chunkSize*8)/peerUpBw); // get obsolete?
-                   ret = m_partnerList->updateChunkAvailTime(holderList[0],
-                                                             copied_expected_set[k],
-                                                             (param_chunkSize*8)/peerUpBw);
-                   if (ret == false)
-                   {
-                      EV << "updateChunkAvailTime == false" << endl;
-                      break;
-                   }
-                } // for
+           if (ret == true)
+           {
+              EV << "ret == true --> erase ... " << endl;
+              // -- Delete the chunk whose supplier had been found
+              std::vector<SEQUENCE_NUMBER_T>::iterator iter;
 
-                // EV << "Browse done!" << endl;
+              iter = std::find(copied_expected_set.begin(), copied_expected_set.end(), seq_num);
+              copied_expected_set.erase(iter);
+           }
 
-                if (ret == true)
-                {
-                   EV << "ret == true --> erase ... " << endl;
-                   // -- Delete the chunk whose supplier had been found
-                   std::vector<SEQUENCE_NUMBER_T>::iterator iter;
-                   // iter = std::find(copied_expected_set.begin(), copied_expected_set.end(), expected_set[i]);
-                   iter = std::find(copied_expected_set.begin(), copied_expected_set.end(), seq_num);
-                   copied_expected_set.erase(iter);
-                }
-
-                // -- Recording results
-                // m_reqChunkId.record(seq_num);
-
-//            } // if (should_be_requested)
+           // -- Recording results
+           // m_reqChunkId.record(seq_num);
         }
-        else
+        else // nHolder > 1
         {
             // -- Add the chunk's sequence number into a suitable dup_set
-            //dup_set[nHolder-2].push_back(expected_set[i]);
             ++count_2;
             dup_set[nHolder-2].push_back(seq_num);
         }
@@ -205,7 +190,6 @@ void DonetPeer::donetChunkScheduling(void)
             candidate2 = holderList[1];
 
             int ret = selectOneCandidate(seq_num, candidate1, candidate2, supplier);
-//            int ret = 0;
 
             if (ret == -1)
             {
@@ -214,18 +198,16 @@ void DonetPeer::donetChunkScheduling(void)
                 continue;
             }
 
-//            if (nHolder > 2)
-//            {
-                for (int j = 2; j < nHolder; ++j)
-                {
-                    // -- User result from the previous calculation
-                    candidate1 = supplier;
-                    // -- Update the second candidate with another partner
-                    //candidate2 = holderList[j];
+            for (int j = 2; j < nHolder; ++j)
+            {
+               // -- User result from the previous calculation
+               candidate1 = supplier;
 
-                    //ret = selectOneCandidate(seq_num, supplier, candidate2, supplier);
-                }
-//            }
+               // -- Update the second candidate with another partner
+               //candidate2 = holderList[j];
+
+               //ret = selectOneCandidate(seq_num, supplier, candidate2, supplier);
+            }
 
             // -- Loops through all holders should be completed at this point
             if (ret == -1)
@@ -235,43 +217,33 @@ void DonetPeer::donetChunkScheduling(void)
                 continue;
             }
 
-//            if (should_be_requested(seq_num) == true)
-//            {
-               // -- Get pointer to the respective NeighborInfo
-//               NeighborInfo *nbr_info = m_partnerList->getNeighborInfo(supplier); // get obsolete?
-               // -- Set the respective element in the SendBm to say that this chunk should be requested
-//               nbr_info->setElementSendBm(seq_num, true); // get obsolete?
+            // -- Set the respective element in the SendBm to say that this chunk should be requested
+            m_partnerList->setElementSendBm(holderList[0], seq_num, true);
 
-                m_partnerList->setElementSendBm(holderList[0], seq_num, true);
+            m_list_requestedChunk.push_back(seq_num);
+            ++m_nChunkRequested_perSchedulingInterval;
 
-               m_list_requestedChunk.push_back(seq_num);
-               ++m_nChunkRequested_perSchedulingInterval;
+            // -- Get peer's upload bandwidth
+            double peerUpBw = m_partnerList->getUpBw(holderList[0]);
 
-               // -- Get peer's upload bandwidth
-//               double peerUpBw = nbr_info->getUpBw(); // get obsolete?
-               double peerUpBw = m_partnerList->getUpBw(holderList[0]);
+            // -- Browse through the expected_set
+            int currentSize = copied_expected_set.size();
+            for (int k = 0; k < currentSize; ++k)
+            {
+               EV << "nHolder > 1; k = " << k << endl;
+               m_partnerList->updateChunkAvailTime(holderList[0],
+                                                   copied_expected_set[k],
+                                                   (param_chunkSize*8)/peerUpBw);
+            }
 
-               // -- Browse through the expected_set
-               int currentSize = copied_expected_set.size();
-               for (int k = 0; k < currentSize; ++k)
-               {
-                  EV << "nHolder > 1; k = " << k << endl;
-//                  nbr_info->updateChunkAvailTime(copied_expected_set[k], (param_chunkSize*8)/peerUpBw); // get obsolete?
-                  m_partnerList->updateChunkAvailTime(holderList[0],
-                                                      copied_expected_set[k],
-                                                      (param_chunkSize*8)/peerUpBw);
-               }
+            // -- Delete the chunk whose supplier had been found
+            // copied_expected_set.erase(expected_set[i]);
+            std::vector<SEQUENCE_NUMBER_T>::iterator iter;
+            iter = std::find(copied_expected_set.begin(), copied_expected_set.end(), seq_num);
+            copied_expected_set.erase(iter);
 
-               // -- Delete the chunk whose supplier had been found
-               // copied_expected_set.erase(expected_set[i]);
-               std::vector<SEQUENCE_NUMBER_T>::iterator iter;
-               iter = std::find(copied_expected_set.begin(), copied_expected_set.end(), seq_num);
-               copied_expected_set.erase(iter);
-
-               // -- Recording results
-               // m_reqChunkId.record(seq_num);
-
-//            } // if (should_be_requested)
+            // -- Recording results
+            // m_reqChunkId.record(seq_num);
 
         } // for (k) -- Browse through all dup_set
     } // for (n) -- Browse through all partners
@@ -287,24 +259,6 @@ void DonetPeer::donetChunkScheduling(void)
 
     // -- Browse through the list of partners to see which one have been set the sendBm
     // -- For each of those one, prepare a suitable ChunkRequestPacket and send to that one
-// obsolete
-//    std::map<IPvXAddress, NeighborInfo*>::iterator iter;
-//    for (iter = m_partnerList->m_map.begin(); iter != m_partnerList->m_map.end(); ++iter)
-//    {
-//        if (iter->second->isSendBmModified() == true)
-//        {
-//            EV << "-------Destination of the ChunkRequestPacket " << iter->first << " :" << endl;
-//            iter->second->printSendBm();
-
-//            MeshChunkRequestPacket *chunkReqPkt = new MeshChunkRequestPacket;
-//                chunkReqPkt->setBitLength(m_appSetting->getPacketSizeChunkRequest());
-//                // -- Map the sendBM into ChunkRequestPacket
-//                iter->second->copyTo(chunkReqPkt);
-
-//            // -- Send the copy
-//            sendToDispatcher(chunkReqPkt, m_localPort, iter->first, m_destPort);
-//        }
-//    } // end of for
 
     // -- Chunk request
     std::map<IPvXAddress, NeighborInfo>::iterator iter;
