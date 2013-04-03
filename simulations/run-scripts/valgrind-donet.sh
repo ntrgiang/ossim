5ddefd6221731d3d3ca79bb5c31abdb592960e9a
Giang Nguyen <nguyen@cs.tu-darmstadt.de>
2013-04-03 18:18:26 +0200

    modified the valgrind-donet.sh to make a profile of the simulation (with callgrind)

--- simulations/run-scripts/valgrind-donet.sh
+++ simulations/run-scripts/valgrind-donet.sh
@@ -1,26 +1,27 @@
 #!/bin/sh
 
 # Root path
-ROOT_PATH=/home/giang/workspace/422-omnet-inet-donet
+ROOT_PATH=/nix/workspace/omnet422-inet118
 
 # Paths for Inet
 INET_PATH=$ROOT_PATH/inet
 INET_SRC_PATH=$INET_PATH/src
 
 # More on the paths
-EXEC_PATH=$ROOT_PATH/scamp
+EXEC_PATH=$ROOT_PATH/so
 EXEC_SIM_PATH=$EXEC_PATH/simulations
 EXEC_SRC_PATH=$EXEC_PATH/src
 
 # Binary file to be executed
-EXEC_FILE=$EXEC_SRC_PATH/scamp
+EXEC_FILE=$EXEC_SRC_PATH/so
 
 # Specify the Network and the configuration file
 NETWORK=Donet_Homo_oneRouter_Network
-CONFIG_FILE=DonetNetworkConfig.ini
+CONFIG_FILE=Donet.ini
 
 #valgrind
-valgrind -v --log-file=valgrind.log --leak-check=full --show-reachable=yes --tool=memcheck --track-origins=yes \
+#valgrind -v --log-file=valgrind.log --leak-check=full --show-reachable=yes --tool=memcheck --track-origins=yes \
+valgrind -v --log-file=valgrind.log --tool=callgrind \
           $EXEC_FILE -u Cmdenv \
 	  -r 0 \
 	  -c $NETWORK \
