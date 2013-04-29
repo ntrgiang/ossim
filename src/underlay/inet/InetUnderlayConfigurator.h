860868e2a0f92d7a9076ee4f3423f9ab89a5d037
Giang Nguyen <nguyen@cs.tu-darmstadt.de>
2013-04-29 05:34:53 +0200

    rearrange code in InetUnderlayConfigurator.h

--- src/underlay/inet/InetUnderlayConfigurator.h
+++ src/underlay/inet/InetUnderlayConfigurator.h
@@ -19,62 +19,62 @@ class IInterfaceTable;
 class IRoutingTable;
 
 
-class InetUnderlayConfigurator: public FlatNetworkConfigurator {
-
-private:
-    std::vector<cModule*> accessRouter; // WATCHOUT!!! vector of pointers
-    std::vector<std::pair<cModule*,double> > accessRoutersWeighted;
-
-    // Access delay bounds for clients
-    double minAccessDelay;
-    double maxAccessDelay;
-public:
-    cTopology topo;
-    NodeInfoVector nodeInfo; // will be of size topo.nodes[]
-    std::map<SimTime, IPAddress> jumps;
-    std::set<cModule*> nodeList;
-    int numIPNodes;
-    int numNodes;
-    int numRouter;
-    int numTerminals;
-    int numSources;
-    int maxNumSources;
-    IInterfaceTable *oldIT;
-    bool noneTransitNodesAccessRouters;
+class InetUnderlayConfigurator: public FlatNetworkConfigurator
+{
+public: // OMNeT++ stuff
+   virtual int numInitStages() const { return 3; }
+   void initialize(int stage);
+   void handleMessage(cMessage* msg);
+   virtual void finish();
 
 public:
-	void addDefaultRoutes(cTopology& topo, NodeInfoVector& nodeInfo, cModule *jumpedNode);
-	void addDefaultRoutes(cTopology& topo, NodeInfoVector& nodeInfo);
-	void addDefaultRoutes();
-	void assignNewAddress(IPAddress oldAddress, int interfaceNbr);
-	void connect(cModule* terminal,bool conn);
-	void deleteOutdatedRoutes();
-	void drop1stInterfaceEntry(cTopology& topo, NodeInfoVector& nodeInfo, cModule *overlayNode, cModule *ppp);
-    void fillRoutingTables(cTopology& topo, NodeInfoVector& nodeInfo, cModule *jumpedNode);
-    void fillRoutingTables(cTopology& topo, NodeInfoVector& nodeInfo);
-    void fillRoutingTables(cModule* terminal, IPAddress destAddr);
-    void getSecondAddress(IPAddress oldAddress, int interfaceNbr);
-    void reAdd1stInterfaceEntry(cTopology& topo, NodeInfoVector& nodeInfo, cModule *overlayNode, cModule *ppp);
-	bool updateRoutes(cModule *jumpedNode, bool multihoming, cModule *modulePPP);
+   cTopology topo;
+   NodeInfoVector nodeInfo; // will be of size topo.nodes[]
+   std::map<SimTime, IPAddress> jumps;
+   std::set<cModule*> nodeList;
+   int numIPNodes;
+   int numNodes;
+   int numRouter;
+   int numTerminals;
+   int numSources;
+   int maxNumSources;
+   IInterfaceTable *oldIT;
+   bool noneTransitNodesAccessRouters;
+
+public:
+   void addDefaultRoutes(cTopology& topo, NodeInfoVector& nodeInfo, cModule *jumpedNode);
+   void addDefaultRoutes(cTopology& topo, NodeInfoVector& nodeInfo);
+   void addDefaultRoutes();
+   void assignNewAddress(IPAddress oldAddress, int interfaceNbr);
+   void connect(cModule* terminal,bool conn);
+   void deleteOutdatedRoutes();
+   void drop1stInterfaceEntry(cTopology& topo, NodeInfoVector& nodeInfo, cModule *overlayNode, cModule *ppp);
+   void fillRoutingTables(cTopology& topo, NodeInfoVector& nodeInfo, cModule *jumpedNode);
+   void fillRoutingTables(cTopology& topo, NodeInfoVector& nodeInfo);
+   void fillRoutingTables(cModule* terminal, IPAddress destAddr);
+   void getSecondAddress(IPAddress oldAddress, int interfaceNbr);
+   void reAdd1stInterfaceEntry(cTopology& topo, NodeInfoVector& nodeInfo, cModule *overlayNode, cModule *ppp);
+   bool updateRoutes(cModule *jumpedNode, bool multihoming, cModule *modulePPP);
 
 protected:
-    IPAddress assignAddress(cModule* terminal);
-    void assignAddresses(cTopology& topo, NodeInfoVector& nodeInfo);
-    cModule* createNode(bool conn);
-    void kfournisseur();
-    void initNodes();
-    void initSources();
-    void getAccessRouter();
-	// TODO
-	// removeOverlayNode(cModule* terminal);
-	void checkRessources();
+   IPAddress assignAddress(cModule* terminal);
+   void assignAddresses(cTopology& topo, NodeInfoVector& nodeInfo);
+   cModule* createNode(bool conn);
+   void kfournisseur();
+   void initNodes();
+   void initSources();
+   void getAccessRouter();
+   // TODO
+   // removeOverlayNode(cModule* terminal);
+   void checkRessources();
 
-public: // OMNeT++ stuff
-    virtual int numInitStages() const { return 6; }
-    void initialize(int stage);
-    void handleMessage(cMessage* msg);
-	virtual void finish();
+private:
+   std::vector<cModule*> accessRouter; // WATCHOUT!!! vector of pointers
+   std::vector<std::pair<cModule*,double> > accessRoutersWeighted;
 
+   // Access delay bounds for clients
+   double minAccessDelay;
+   double maxAccessDelay;
 };
 
 #endif /* INETUNDERLAYCONFIGURATOR_H_ */
