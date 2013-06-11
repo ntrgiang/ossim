5165f66f28ad564c0ce2fbca3bcbafa19faaf7f7
Giang Nguyen <nguyen@cs.tu-darmstadt.de>
2013-06-11 17:51:57 +0200

    reverted back to the version without traceroute

--- src/global/topology/PPEdge.cc
+++
@@ -1,8 +0,0 @@
-#include "PPEdge.h"
-
-PPEdge::PPEdge(IPvXAddress begin, IPvXAddress end, double beginJoinTime, double endJoinTime) {
-    _begin         = begin;
-    _end           = end;
-    _beginJoinTime = beginJoinTime;
-    _endJoinTime   = endJoinTime;
-}
