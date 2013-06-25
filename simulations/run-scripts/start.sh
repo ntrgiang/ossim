200b62bc879508aa82dcd1807fee677d8a1607b1
Giang Nguyen <nguyen@cs.tu-darmstadt.de>
2013-06-25 04:50:09 +0200

    Fixed the bugs when sync'ing files from the inet branch:    * added the paths to find NED files of INET    * fixed one typo in the config to run Donet with Cmdenv    * revert the use of ForwarderTopo back to Forwarder in the DonetPeerApp and DonetSourceApp

--- simulations/run-scripts/start.sh
+++ simulations/run-scripts/start.sh
@@ -5,12 +5,12 @@
 # $1 = Configuration name
 # $2 = INI file which stores the configuration
 
-config=Donet_oneRouter_underlayAttack 
-ini_file=../Donet-underlayAttack.ini
+#config=Donet_oneRouter_underlayAttack 
+#ini_file=../Donet-underlayAttack.ini
 
-#config=$1
-#ini_file=$2
+config=$1
+ini_file=$2
 
 
 
-opp_runall -j1 ../../src/so -r 0 -u Cmdenv -c $config $ini_file
+opp_runall -j1 ../../src/so -r 0 -u Cmdenv -c $config $ini_file -n ../:../../src:../../../inet
